840a8f907270d5160dcef321d89a598a72f1da8a
Reza <reza.hasani7899@gmail.com>
2021-10-23 01:18:33 +0330

    Added required configs to .umirc.ts file

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  favicon:
+    'https://admin.soltia.com/uploads/thumbnail_cropped_favicon_soltia_1_192x192_76db7a2462.png',
   nodeModulesTransform: {
     type: 'none',
   },
   routes: [
-    { path: '/', component: '@/pages/index' },
+    {
+      path: '/',
+      component: '@/pages/index',
+      title: 'Soltia – A home for amazing people with amazing skills',
+    },
   ],
+  styles: [
+    `https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap`,
+  ],
+  ssr: {},
   fastRefresh: {},
 });
