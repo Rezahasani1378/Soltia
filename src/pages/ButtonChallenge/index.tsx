54fc446f21152586b8420b5ca584f84c441fc46e
Reza <reza.hasani7899@gmail.com>
2021-10-25 20:23:51 +0330

    Added delete button to the center of /challenge route

--- src/pages/ButtonChallenge/index.tsx
+++ src/pages/ButtonChallenge/index.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  padding: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #F1A85C;
+  font-size: 20px;
+  font-weight: bold;
+  outline: none;
+  border: 2px #989898 solid;
+  cursor: pointer;
+
+  &:after {
+    width: 60px;
+    height: 60px;
+    content: '\\2715';
+    font-size: 35px;
+    margin-left: 14px;
+    background-color: white;
+    border-radius: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
 
 const ButtonChallenge = () => {
   return (
-    <></>
+    <Button>Delete</Button>
   )
 }
 
